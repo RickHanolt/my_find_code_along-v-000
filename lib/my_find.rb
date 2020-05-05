f372fea47e0fe414e0ba7da4ff78e81f468102ce
Richard Hanolt <rhanolt@gmail.com>
2020-05-05 21:08:00 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(collection[1])
-      return yield(collection[1])
+    if yield(collection[i])
+      return yield(collection[i])
     end
     i += 1
   end
